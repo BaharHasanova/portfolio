96223c4e10437383002651c58defa392d10d0bc1
BaharHasanova <SW01081496@student.uniten.edu.my>
2024-09-09 17:01:33 +0800

    feat:Add animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,15 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/main_photo.jpeg";
+import profilePic from "../assets/main_photo.jpg";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+	hidden: { x: -100, opacity: 0 },
+	visible: {
+		x: 0,
+		opacity: 1,
+		transition: { duration: 0.5, delay: delay },
+	},
+});
 
 const Hero = () => {
 	return (
@@ -8,25 +18,46 @@ const Hero = () => {
 				{/* Main Content Section */}
 				<div className="w-full lg:w-1/2">
 					<div className="flex flex-col items-center lg:items-start">
-						<h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+						<motion.h1
+							variants={container(0)}
+							initial="hidden"
+							animate="visible"
+							className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+						>
 							Bahar Hasanova
-						</h1>
-						<span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+						</motion.h1>
+						<motion.span
+							variants={container(0.5)}
+							initial="hidden"
+							animate="visible"
+							className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
+						>
 							Final Year Computer Science Student
-						</span>
-						<p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+						</motion.span>
+						<motion.p
+							variants={container(1)}
+							initial="hidden"
+							animate="visible"
+							className="my-2 max-w-xl py-6 font-light tracking-tighter"
+						>
 							{HERO_CONTENT}
-						</p>
+						</motion.p>
 					</div>
 				</div>
 
 				{/* Image Section */}
 				<div className="w-full lg:w-1/2 lg:p-8 flex justify-center items-center">
-					<img
+					<motion.img
+						initial={{ x: 100, opacity: 0 }}
+						animate={{ x: 0, opacity: 1 }}
+						transition={{ duration: 1, delay: 1.2 }}
 						src={profilePic}
 						alt="Bahar"
-						className="transform rotate-90 rounded-2xl" // Rotate the image
-						style={{ maxWidth: "80%", height: "auto" }} // Ensure image scales properly
+						className="rounded-2xl" // Keep rounded corners
+						style={{
+							maxWidth: "80%",
+							height: "auto",
+						}} // Rotate the image 180 degrees
 					/>
 				</div>
 			</div>
